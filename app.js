c9abb6b268afbbd983ebfe7e4214951fa073c413
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-02-13 17:45:02 +0530

    asd

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ app.post('/upload', function (req, res) {
         } else if (err) {
             return res.status(500).json(err)
         }
+        req.file.filename = req.file.filename.toLowerCase()
         if (req.file.filename.endsWith('.pdf')) {
             console.log(execFileSync('/usr/bin/convert', ['./public/' + req.file.filename + '[0]', './public/' + req.file.filename + '.thumbnail.png']).toString('utf8'))
         } else if (req.file.filename.endsWith('.jpg') || req.file.filename.endsWith('.jpeg') || req.file.filename.endsWith('.png')) {

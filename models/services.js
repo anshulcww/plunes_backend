c579e3dcb4df05793b46974622649da37e51400b
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-02-14 12:02:36 +0530

    asd

---
+++ models/services.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const fs = require('fs')
+
+const ObjectId = mongoose.Types.ObjectId
+
+const servicesSchema = mongoose.Schema({
+    speciality: String,
+    specialityId: ObjectId,
+    service: String,
+    details: String,
+    duration: Number,
+    sittings: Number,
+    dnd: String,
+    tags: String,
+    category: String
+}, {timestamp: true})
+
+const Services = mongoose.model('services', servicesSchema)
+
+module.exports = Services

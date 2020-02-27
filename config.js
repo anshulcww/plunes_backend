dc8050e21064f6aeab867e8a612f066c05064d60
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-02-27 11:46:34 +0530

    asd

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
     MONGODB_URL: 'mongodb://172.31.19.76:27017/plunesdb2',
     ELASTIC_URL: "http://172.31.41.184:9200",
     RAZORPAY_APP_URL: 'https://plunes.co',
-    COUPON_CODES: ['SPAZE10000', 'NILE10000', 'UPPAL10000', 'FEB10000']
+    COUPON_CODES: ['SPAZE10000', 'NILE10000', 'UPPAL10000', 'FEB10000', 'GREENS10000']
 }

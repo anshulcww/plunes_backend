7dea59750ad9fcec5811a35f50deca605e8aeeb3
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-02-27 17:45:09 +0530

    asd

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ module.exports = {
     ELASTIC_URL: "http://172.31.41.184:9200",
     RAZORPAY_APP_URL: 'https://plunes.co',
     COUPON_CODES: ['SPAZE10000', 'NILE10000', 'UPPAL10000', 'FEB10000', 'GREENS10000'],
-    ES_INDEX: "services"
+    ES_INDEX: "services",
+    ENVIRONMENT: "production"
 }

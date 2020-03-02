380d710b6cabbe468faf90d2064c7e4893504547
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-03-02 18:17:49 +0530

    asd

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     PORT: 5000,
     JWT_KEY: 'Make my day!',
-    MONGODB_URL: 'mongodb://172.31.19.76:27017/plunesdb2',
+    MONGODB_URL: 'mongodb://localhost:27017/plunesdb2',
     ELASTIC_URL: "http://172.31.41.184:9200",
     RAZORPAY_APP_URL: 'https://plunes.co',
     COUPON_CODES: ['SPAZE10000', 'NILE10000', 'UPPAL10000', 'FEB10000', 'CAREERS360'],
-    ES_INDEX: "services",
-    ENVIRONMENT: "production"
+    ES_INDEX: "services_development",
+    ENVIRONMENT: "development"
 }

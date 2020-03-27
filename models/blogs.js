888e81be586b7203e0261396d14ea5a036c2ce5b
Divyanshu Sharma <divyanshu.sharma@plunes.com>
2020-03-27 11:42:26 +0530

    asd

--- models/blogs.js
+++ models/blogs.js
@@ -61,7 +61,7 @@ blogSchema.statics.deletePost = title => {
 blogSchema.statics.getPostList = () => {
     return new Promise(async (resolve, reject) => {
         try {
-            let result = await Blog.find().sort({ _id: -1 })
+            let result = await Blog.find({}, '_id title createdAt description imageUrl').sort({ _id: -1 })
             resolve(result)
         } catch (e) {
             reject(e)
